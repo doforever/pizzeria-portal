a137f59c671c1bee0b0741168676282d2c89a05f
Dorota Tomala <doforever@gmail.com>
2021-03-05 14:57:15 +0100

    Include materialUI AppBar and Toolbar

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Container from '@material-ui/core/Container';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const MainLayout = ({children}) => (
   <div>
-    <PageNav />
-    <main>
+    <AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar disableGutters>
+          <PageNav />
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Container maxWidth='lg'>
+      <Toolbar />
       {children}
-    </main>
+    </Container>
   </div>
 );
 
