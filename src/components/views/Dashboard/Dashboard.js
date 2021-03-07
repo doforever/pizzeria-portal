e0374998cfa5a1e84b5fa1b352138f5c1860ca6a
Dorota Tomala <doforever@gmail.com>
2021-03-07 16:23:21 +0100

    Fill Dashboard with materialUI components

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -1,9 +1,74 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
 
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 const Dashboard = () => (
   <div className={styles.component}>
-    <h2>Dashboard view</h2>
+    <Typography component="h1" variant="h3" gutterBottom>Dashboard</Typography>
+    <Grid container spacing="2">
+      <Grid item xs='12' >
+        <Card>
+          <CardContent>
+            <Typography component="h2" variant="h4" gutterBottom>Coming events</Typography>
+            <List>
+              <ListItem button>
+                <ListItemText primary="12:30-12:30 Table 1" secondary="starters: water, bread"/>
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="12:30-12:30 Table 1" />
+              </ListItem>
+            </List>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs='12' >
+        <Card>
+          <CardContent>
+            <Typography component="h2" variant="h4" gutterBottom>Order statistics</Typography>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Orders</TableCell>
+                    <TableCell align="right">Restaurant</TableCell>
+                    <TableCell align="right">Delivery</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      Order count
+                    </TableCell>
+                    <TableCell align="right">3</TableCell>
+                    <TableCell align="right">4</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      Income
+                    </TableCell>
+                    <TableCell align="right">$300</TableCell>
+                    <TableCell align="right">$400</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   </div>
 );
 
--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -3,33 +3,35 @@ import styles from './Login.module.scss';
 
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';import TextField from '@material-ui/core/TextField';
 
 const Login = () => (
   <div className={styles.component}>
-    <Grid container justify='center'alignContent='center'>
+    <Grid container justify='center' alignContent='center'>
       <Grid item xs='12' sm='8' md='6'>
-        <Paper className={styles.content}>
-          <form noValidate>
-            <TextField
-              required
-              id="login"
-              label="Login"
-              variant="outlined"
-              fullWidth
-              margin='normal'/>
-            <TextField
-              required
-              id="password"
-              label="Password"
-              type="password"
-              variant="outlined"
-              fullWidth
-              margin='normal'/>
-            <Button className={styles.submit} variant='contained' color='primary' href='/panel'>Log in</Button>
-          </form>
-        </Paper>
+        <Card>
+          <CardContent>
+            <form noValidate>
+              <TextField
+                required
+                id="login"
+                label="Login"
+                variant="outlined"
+                fullWidth
+                margin='normal'/>
+              <TextField
+                required
+                id="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                margin='normal'/>
+              <Button className={styles.submit} variant='contained' color='primary' href='/panel'>Log in</Button>
+            </form>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   </div>
