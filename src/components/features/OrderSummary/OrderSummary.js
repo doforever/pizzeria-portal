f63723d2e23aed00e67ccdf1a1bb8bf58057813e
Dorota Tomala <doforever@gmail.com>
2021-03-11 00:33:19 +0100

    Create OrderSummary and OrderParams

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import order from '../../../data/order.json';
 
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import { DataGrid } from '@material-ui/data-grid';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import OrderParams from '../OrderParams/OrderParams';
+
+import styles from './OrderSummary.module.scss';
 
 const OrderSummary = () => {
-  const {totalPrice, id, table, products} = order;
+  const {totalPrice, id, products} = order;
+  const [isReadOnly, setReadOnly] = useState(true);
+
+  const renderAmount = (value) => (
+    <TextField className={styles.amount}
+      value={value}
+      type="number"
+      InputProps={{
+        readOnly: isReadOnly,
+      }}
+    />
+  );
+
+  const renderActions = () => (
+    <div>
+      <IconButton aria-label="edit" onClick={() => setReadOnly(false)}>
+        <EditIcon fontSize="small"/>
+      </IconButton>
+      <IconButton aria-label="delete">
+        <DeleteIcon fontSize="small"/>
+      </IconButton>
+    </div>
+  );
+
+  const columns = [
+    { field: 'id', headerName: 'ID', flex:1},
+    { field: 'params', headerName: 'Params', flex: 3, renderCell: ({row}) => OrderParams(row.params)},
+    { field: 'priceSingle', headerName: 'Price', flex: 1},
+    { field: 'amount', headerName: 'Amount', flex: 1, renderCell: ({row}) => renderAmount(row.amount)},
+    { field: 'actions', headerName: 'Actions', flex:1, renderCell: () => renderActions()},
+  ];
 
   return (
-    <div>
-      <Typography variant="h4" gutterBottom>Order id: {id}</Typography>
-      <Typography variant="h4" gutterBottom>Total price: {totalPrice}</Typography>
-    </div>
+    <Paper variant="outlined" className={styles.component}>
+      <Grid container spacing={2} direction="column">
+        <Grid item>
+          <Typography component="h3" variant="h5" gutterBottom>Order summary</Typography>
+        </Grid>
+        <Grid item>
+          <Typography component="h4" variant="h6" >Order id: {id}</Typography>
+        </Grid>
+        <Grid item>
+          <DataGrid
+            rows={products}
+            columns={columns}
+            autoHeight
+            disableColumnSelector
+            disableSelectionOnClick
+            disableColumnMenu
+            hideFooter
+            headerHeight={36}
+            rowHeight={130}
+          />
+        </Grid>
+        <Grid item>
+          <Typography component="h4" variant="h6" >Total price: {totalPrice}</Typography>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
